7066a20dbf024b57d22d2c5420ae1d9ca218195e
Sebbe <sebban952@gmail.com>
2026-04-23 18:34:37 +0200

    adding finished project :)

---
+++ CRM System/src/main/java/se/yrgo/services/calls/CallHandlingServiceImpl.java
@@ -0,0 +1,32 @@
+package se.yrgo.services.calls;
+
+import se.yrgo.domain.Action;
+import se.yrgo.domain.Call;
+import se.yrgo.services.customers.CustomerManagementService;
+import se.yrgo.services.customers.CustomerNotFoundException;
+import se.yrgo.services.diary.DiaryManagementService;
+
+import java.util.Collection;
+
+public class CallHandlingServiceImpl implements CallHandlingService {
+    private CustomerManagementService cms;
+    private DiaryManagementService dms;
+
+    public void setCms(CustomerManagementService cms) {
+        this.cms = cms;
+    }
+
+    public void setDms(DiaryManagementService dms) {
+        this.dms = dms;
+    }
+
+    @Override
+    public void recordCall(String customerId, Call newCall, Collection<Action> actions) throws CustomerNotFoundException {
+        cms.recordCall(customerId, newCall);
+
+        for(Action action : actions) {
+            dms.recordAction(action);
+        }
+
+    }
+}
